6f7fe78ab8826cab8c547111fd149246f9928d2d
AroSaneD <mail.junk.3135@gmail.com>
2021-12-02 03:15:23 +0200

    More minor fixes

--- src/services/filestream.service.ts
+++ src/services/filestream.service.ts
@@ -6,18 +6,23 @@ export class FileStreamService {
     writeFileSync(writePath: string, contents: string) {
         return fs.writeFileSync(writePath, contents, 'utf-8');
     }
+
     readFileSync(origFilePath: string): string {
         return fs.readFileSync(origFilePath, 'utf-8');
     }
+
     statSync(origFilePath: string) {
         return fs.statSync(origFilePath);
     }
+
     readdirSync(templatePath: string) {
         return fs.readdirSync(templatePath);
     }
+
     mkdirSync(projectPath: string) {
         return fs.mkdirSync(projectPath);
     }
+
     existsSync(projectPath: string) {
         return fs.existsSync(projectPath);
     }
